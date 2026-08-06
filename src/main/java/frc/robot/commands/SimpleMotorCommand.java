bf7563f2f2b283d45f0d11ec50c2738fb8105f95
McRoil <liorpit12@gmail.com>
2026-08-06 18:31:45 +0300

    Finished everything

---
+++ src/main/java/frc/robot/commands/SimpleMotorCommand.java
@@ -0,0 +1,57 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.SimpleMotorSubsystem;
+
+public class SimpleMotorCommand extends Command{
+
+    // required variables:
+    private final SimpleMotorSubsystem subsystem;
+    private final double power;
+    private final double duration;
+    private double startTime = 0;
+    private Timer timer = new Timer();
+
+
+
+
+    // constructor
+    public `SimpleMotorCommand(SimpleMotorSubsystem subsystem, double power, double duration) {
+        this.subsystem = subsystem;
+        this.power = power;
+        this.duration = duration;
+
+    }
+
+
+    // initialize
+    @Override
+    public void initialize() {
+        startTime = Timer.getFPGATimestamp();
+        timer.restart();
+        System.out.println("Command started at: " + startTime);
+        System.out.println("Seconds for: " + duration + "second/s with power: " + power);
+
+    }
+
+
+    // execute
+    @Override
+    public void execute() {
+        subsystem.setPower(power);
+    }
+
+    // is finished?
+    @Override
+    public boolean isFinished() {
+        return Timer.getFPGATimestamp() >= startTime + duration;
+    }
+
+    // end
+    public void end(boolean interrupted) {
+        subsystem.stop();
+        System.out.println("Command ended at: " + Timer.getFPGATimestamp());
+    }
+
+}
---
+++ src/main/java/frc/robot/subsystems/SimpleMotorSubsystem.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+
+public class SimpleMotorSubsystem extends SubsystemBase {
+    private final TalonFX motor;
+
+
+    public SimpleMotorSubsystem() {
+        motor = new TalonFX(Constants.OperatorConstants.MOTOR_ID,Constants.OperatorConstants.MOTOR_CANBUS);
+    }
+
+
+
+    // set motor power
+    public void setPower(double power) {
+        motor.set(power);
+    }
+
+
+    // stop the motor
+    public void stop() {
+        setPower(0);
+    }
+}
