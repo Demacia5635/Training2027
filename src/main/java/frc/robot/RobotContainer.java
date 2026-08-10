a8ca0b6b44ca4fee02879597fd397a29da78eb3c
noamkabiri <noam.kabiri@gmail.com>
2026-08-10 20:05:52 +0300

    working on the homework

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.SimpleMotorCommand;
+import frc.robot.commands.SteerToAngle;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.SimpleMotorSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -61,6 +62,7 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
     // return Autos.exampleAuto(m_exampleSubsystem);
-    return new SimpleMotorCommand(simpleMotorSubsystem, 0.5,0.7, 3.0);
+    // return new SimpleMotorCommand(simpleMotorSubsystem, 0.5,0.7, 3.0);
+    return new SteerToAngle(simpleMotorSubsystem, 90.0);
   }
 }
--- src/main/java/frc/robot/subsystems/SimpleMotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/SimpleMotorSubsystem.java
@@ -2,40 +2,65 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class SimpleMotorSubsystem extends SubsystemBase { 
-private final TalonFX SteerMotor;
-private final TalonFX DriveMotor;
+public class SimpleMotorSubsystem extends SubsystemBase {
+    private final TalonFX SteerMotor;
+    private final TalonFX DriveMotor;
 
-public SimpleMotorSubsystem() {
-    super();
-    this.SteerMotor = new TalonFX( Constants.OperatorConstants.SteerMotor_id, Constants.OperatorConstants.Canbus);
-    this.DriveMotor = new TalonFX( Constants.OperatorConstants.DriveMotor_id, Constants.OperatorConstants.Canbus);
+    public SimpleMotorSubsystem() {
+        super();
+        this.SteerMotor = new TalonFX(Constants.OperatorConstants.SteerMotor_id, Constants.OperatorConstants.Canbus);
+        this.DriveMotor = new TalonFX(Constants.OperatorConstants.DriveMotor_id, Constants.OperatorConstants.Canbus);
 
-}
-public void setSteerPower(double power) {
-    SteerMotor.set(power);
+    }
 
-}
+    public void setSteerPower(double power) {
+        SteerMotor.set(power);
 
+    }
 
-public void stopSteer() {
-    setSteerPower(0);
+    public double getSteerPower() {
+        return SteerMotor.get();
+    }
 
-}
+    public double getDrivePower() {
+        return DriveMotor.get();
+    }
 
-public void setDrivePower(double power) {
-    SteerMotor.set(power);
-    DriveMotor .set(power);
+    public void stopSteer() {
+        setSteerPower(0);
 
-}
+    }
 
+    public void setDrivePower(double power) {
+        SteerMotor.set(power);
+        DriveMotor.set(power);
 
-public void stopDrive() {
-    setDrivePower(0);
+    }
 
-}
+    public void stopDrive() {
+        setDrivePower(0);
 
+    }
+    
+    public void setAngle(double angle) {
+        SteerMotor.setPosition(angle);
+    }
+    public double getSteerMotorPosition() {
+        return SteerMotor.getPosition().getValueAsDouble();
+    }
+    public double getDriveMotorPosition() {
+        return DriveMotor.getPosition().getValueAsDouble();
+    }
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Steer Power", getSteerPower());
+        SmartDashboard.putNumber("Drive Power", getDrivePower());
+        SmartDashboard.putNumber("Steer Position", getSteerMotorPosition());
+        SmartDashboard.putNumber("Drive Position", getDriveMotorPosition());
+
+    }
 }
