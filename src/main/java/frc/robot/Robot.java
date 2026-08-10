6afbfd38ae6ad8e6f2bb422eea6d7f1e1e2b3703
danik-lab <danik.gdbg@outlook.com>
2026-08-10 17:47:30 +0300

    Initial commit of project structure and essential files

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
 
@@ -16,7 +17,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void robotPeriodic() {
-
+      CommandScheduler.getInstance().run();
     }
 
     @Override
