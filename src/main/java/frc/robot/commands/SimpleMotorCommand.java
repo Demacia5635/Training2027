bf6984d0d22b7ba61d93c19ee3ec5e724b27d099
danik-lab <danik.gdbg@outlook.com>
2026-08-10 17:16:52 +0300

    Refactor motor control structure and remove example commands and subsystems

---
+++ src/main/java/frc/robot/commands/SimpleMotorCommand.java
@@ -0,0 +1,65 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.SimpleMotorSubsystem;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class SimpleMotorCommand extends Command {
+
+    private final SimpleMotorSubsystem subsystem;
+
+    private final double motor1Power;
+    private final double motor2Power;
+    private final double duration;
+
+    private final Timer timer = new Timer();
+
+    public SimpleMotorCommand(
+            SimpleMotorSubsystem subsystem,
+            double motor1Power,
+            double motor2Power,
+            double duration) {
+
+        this.subsystem = subsystem;
+        this.motor1Power = motor1Power;
+        this.motor2Power = motor2Power;
+        this.duration = duration;
+
+        addRequirements(subsystem);
+    }
+
+    @Override
+    public void initialize() {
+
+        timer.restart();
+
+        System.out.println(
+            "SimpleMotorCommand started"
+        );
+    }
+
+    @Override
+    public void execute() {
+
+        subsystem.setMotor1Power(motor1Power);
+        subsystem.setMotor2Power(motor2Power);
+
+    }
+
+    @Override
+    public boolean isFinished() {
+
+        return timer.hasElapsed(duration);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+        subsystem.stop();
+
+        System.out.println(
+            "SimpleMotorCommand ended"
+        );
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/SimpleMotorSubsystem.java
@@ -0,0 +1,38 @@
+package frc.robot.subsystems;
+import frc.robot.Constants;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SimpleMotorSubsystem extends SubsystemBase {
+
+    private final TalonFX motor1;
+    private final TalonFX motor2;
+
+    public SimpleMotorSubsystem() {
+
+        super();
+
+        motor1 = new TalonFX(
+            Constants.SimpleMotorConstants.Motor1ID,
+            Constants.SimpleMotorConstants.MotorCANbus
+        );
+
+        motor2 = new TalonFX(
+            Constants.SimpleMotorConstants.Motor2ID,
+            Constants.SimpleMotorConstants.MotorCANbus
+        );
+    }
+
+    public void setMotor1Power(double power) {
+        motor1.set(power);
+    }
+
+    public void setMotor2Power(double power) {
+        motor2.set(power);
+    }
+
+    public void stop() {
+        motor1.set(0);
+        motor2.set(0);
+    }
+}
