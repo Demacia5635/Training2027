bb7e3fa6d045b0ceb64f1175cc70caea0896c9db
danik-lab <danik.gdbg@outlook.com>
2026-08-20 20:51:07 +0300

    Refactor SimpleMotorSubsystem to improve motor control; streamline methods for drive and steer motors, enhance SmartDashboard integration, and ensure consistent data access.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,16 +1,18 @@
 package frc.robot;
 
-import java.security.PublicKey;
-
-import frc.demacia.utils.motors.TalonFXConfig;
 import frc.demacia.utils.motors.BaseMotorConfig.Canbus;
+import frc.demacia.utils.motors.TalonFXConfig;
 
 public final class Constants {
-    
+    private Constants() {}
+
     public static final class SimpleMotorConstants {
+        private SimpleMotorConstants() {}
+
         public static final int MOTOR_DRIVE = 1;
         public static final int MOTOR_STEER = 2;
         public static final Canbus CAN_BUS = Canbus.Rio;
+
         public static final double DRIVE_KP = 0.0;
         public static final double DRIVE_KI = 0.0;
         public static final double DRIVE_KD = 0.0;
@@ -27,13 +29,18 @@ public final class Constants {
         public static final double STEER_KA = 0.0;
         public static final double STEER_KG = 0.0;
 
+        public static final TalonFXConfig DRIVE_CONFIG =
+            new TalonFXConfig(MOTOR_DRIVE, CAN_BUS, "Drive Motor")
+                .withPID(
+                    DRIVE_KP, DRIVE_KI, DRIVE_KD,
+                    DRIVE_KS, DRIVE_KV, DRIVE_KA, DRIVE_KG
+                );
 
-
-        public static final TalonFXConfig DRIVE_CONFIG = new TalonFXConfig(MOTOR_DRIVE, CAN_BUS, "Drive Motor")
-        .withPID(DRIVE_KP, DRIVE_KI, DRIVE_KD, DRIVE_KS);
-        public static final TalonFXConfig STEER_CONFIG = new TalonFXConfig(MOTOR_STEER, CAN_BUS, "Steer Motor")
-        .withPID(STEER_KP, STEER_KI, STEER_KD, STEER_KS);
-    
-
+        public static final TalonFXConfig STEER_CONFIG =
+            new TalonFXConfig(MOTOR_STEER, CAN_BUS, "Steer Motor")
+                .withPID(
+                    STEER_KP, STEER_KI, STEER_KD,
+                    STEER_KS, STEER_KV, STEER_KA, STEER_KG
+                );
     }
 }
--- src/main/java/frc/robot/subsystems/SimpleMotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/SimpleMotorSubsystem.java
@@ -1,98 +1,134 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.demacia.utils.Data;
 import frc.demacia.utils.motors.TalonFXMotor;
 import frc.robot.Constants;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 public class SimpleMotorSubsystem extends SubsystemBase {
-
-    // Motor 1 = DRIVE
-    private final TalonFXMotor motor1;
-
-    // Motor 2 = STEER
-    private final TalonFXMotor motor2;
+    private final TalonFXMotor driveMotor;
+    private final TalonFXMotor steerMotor;
 
     public SimpleMotorSubsystem() {
+        driveMotor =
+            new TalonFXMotor(
+                Constants.SimpleMotorConstants.DRIVE_CONFIG
+            );
 
-        super();
+        steerMotor =
+            new TalonFXMotor(
+                Constants.SimpleMotorConstants.STEER_CONFIG
+            );
 
-        TalonFXMotor_drive = new TalonFXMotor(Constants.CONFIG
-
-        );
-
-        motor2 = new TalonFXMotor(
-            Constants.SimpleMotorConstants.Motor2ID,
-            Constants.SimpleMotorConstants.MotorCANbus
-        );
+        // Lets Elastic show the motor telemetry sendables.
+        SmartDashboard.putData("Simple Motors/Drive", driveMotor);
+        SmartDashboard.putData("Simple Motors/Steer", steerMotor);
     }
 
 
-    // =========================
-    // MOTOR 1 - DRIVE
-    // =========================
+    public void setDrivePower(double power) {
+        driveMotor.setDuty(MathUtil.clamp(power, -1.0, 1.0));
+    }
+
+    public double getDrivePosition() {
+        return driveMotor.getCurrentPosition();
+    }
+
+    public double getDriveVelocity() {
+        return driveMotor.getCurrentVelocity();
+    }
+
+    public double getDriveCurrent() {
+        return driveMotor.getCurrentCurrent();
+    }
+
+    // Steer motor
+
+    public void setSteerPower(double power) {
+        steerMotor.setDuty(MathUtil.clamp(power, -1.0, 1.0));
+    }
+
+    public double getSteerPosition() {
+        return steerMotor.getCurrentPosition();
+    }
+
+    public double getSteerVelocity() {
+        return steerMotor.getCurrentVelocity();
+    }
+
+    public double getSteerCurrent() {
+        return steerMotor.getCurrentCurrent();
+    }
+
+    // Compatibility with your existing SimpleMotorCommand 
 
     public void setMotor1Power(double power) {
-
-        motor1.set(power);
+        setDrivePower(power);
     }
-
 
     public double getMotor1Pos() {
-
-        return motor1
-            .getPosition()
-            .getValueAsDouble();
+        return getDrivePosition();
     }
-
-
-    // =========================
-    // MOTOR 2 - STEER
-    // =========================
 
     public void setMotor2Power(double power) {
-
-        motor2.set(power);
+        setSteerPower(power);
     }
-
 
     public double getMotor2Pos() {
-
-        return motor2
-            .getPosition()
-            .getValueAsDouble();
+        return getSteerPosition();
     }
 
+    // Demacia Data access
 
-    // =========================
-    // STOP BOTH MOTORS
-    // =========================
+    public Data<Angle> getDrivePositionData() {
+        return driveMotor.getPositionSignal();
+    }
+
+    public Data<AngularVelocity> getDriveVelocityData() {
+        return driveMotor.getVelocitySignal();
+    }
+
+    public Data<Current> getDriveCurrentData() {
+        return driveMotor.getCurrentSignal();
+    }
+
+    public Data<Angle> getSteerPositionData() {
+        return steerMotor.getPositionSignal();
+    }
+
+    public Data<AngularVelocity> getSteerVelocityData() {
+        return steerMotor.getVelocitySignal();
+    }
+
+    public Data<Current> getSteerCurrentData() {
+        return steerMotor.getCurrentSignal();
+    }
 
     public void stop() {
-
-        motor1.set(0);
-        motor2.set(0);
+        driveMotor.stop();
+        steerMotor.stop();
     }
 
-
-    // =========================
-    // ELASTIC
-    // =========================
+    public void checkElectronics() {
+        driveMotor.checkElectronics();
+        steerMotor.checkElectronics();
+    }
 
     @Override
     public void periodic() {
+        // Data.refreshAll() is already called by Demacia's LogManager.
+        // These getters therefore use the refreshed cached Data values.
+        SmartDashboard.putNumber("Motor 1 Position", getDrivePosition());
+        SmartDashboard.putNumber("Motor 1 Velocity", getDriveVelocity());
+        SmartDashboard.putNumber("Motor 1 Current", getDriveCurrent());
 
-        SmartDashboard.putNumber(
-            "Motor 1 Position",
-            getMotor1Pos()
-        );
-
-        SmartDashboard.putNumber(
-            "Motor 2 Position",
-            getMotor2Pos()
-        );
+        SmartDashboard.putNumber("Motor 2 Position", getSteerPosition());
+        SmartDashboard.putNumber("Motor 2 Velocity", getSteerVelocity());
+        SmartDashboard.putNumber("Motor 2 Current", getSteerCurrent());
     }
 }
