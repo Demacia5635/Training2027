e9b7f3da047b58cbb72b9d968af9dfb809087c86
cyotam <c.yotam123@gmail.com>
2026-08-24 17:35:45 +0300

    added config and created a subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+import frc.demacia.utils.motors.TalonFXConfig;
+import frc.demacia.utils.motors.BaseMotorConfig.Canbus;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -15,5 +18,24 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  }
+
+  public static class SwerveModuleConstants {
+    public static final int DRIVE_MOTOR_ID = 1;
+    public static final int STEER_MOTOR_ID = 2;
+    public static final Canbus DRIVE_CANBUS = Canbus.Rio;
+    public static final Canbus STEER_CANBUS = Canbus.Rio;
+    public static final String DRIVE_MOTOR_NAME = "driveMotor";
+    public static final String STEER_MOTOR_NAME = "steerMotor";
+    public static final double DRIVE_DIAMETER = 4 * 0.0254;
+    public static final double DRIVE_GEAR_RATIO = 8.14;
+    public static final double STEER_GEAR_RATIO = 150d / 7d;
+
+    public static final TalonFXConfig DRIVE_MOTOR_CONFIG = new TalonFXConfig(DRIVE_MOTOR_ID, DRIVE_CANBUS, DRIVE_MOTOR_NAME)
+    .withBrake(true)
+    .withMeterMotor(DRIVE_GEAR_RATIO, DRIVE_DIAMETER);
+    public static final TalonFXConfig STEER_MOTOR_CONFIG = new TalonFXConfig(STEER_MOTOR_ID, STEER_CANBUS, STEER_MOTOR_NAME)
+    .withBrake(true)
+    .withRadiansMotor(STEER_GEAR_RATIO);
   }
 }
