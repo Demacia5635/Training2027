1b2b15535bdd22c5a2382a73c6c26fe791fd0e6e
Harel Kogan <koganharel@gmail.com>
2026-08-24 18:06:43 +0300

    maby

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,19 +5,26 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+  public static final class SwerveModuleConstants {
+    public static final int MOTOT_ID = 1;
+    public static final String MotorCANbus = "rio";
+    public static final String Name ="";
+  }
+
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
-    public static final int MOTOR_ID = 2;
-    public static final int MOTOR_ID2 = 1;
-    public static final String MotorCANbus = "rio";
-
   }
 }
