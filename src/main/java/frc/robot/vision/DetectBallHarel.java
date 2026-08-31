06025c32bfb617eed7c450aeb54618c8ee1274bb
Harel Kogan <koganharel@gmail.com>
2026-08-31 17:33:01 +0300

    hafsaka

---
+++ src/main/java/frc/robot/vision/DetectBallHarel.java
@@ -0,0 +1,107 @@
+package frc.robot.vision;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.highgui.HighGui;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
+
+public class DetectBallHarel {
+
+    public static void main(String[] args) {
+        String fileName = "detectBallExercise.jpg";
+        System.load(System.getProperty("user.dir") + "\\opencv_java4140.dll");
+        Mat img = Imgcodecs.imread(fileName);
+        Mat resized = new Mat();
+        Imgproc.resize(img, resized, new Size(640, 480));
+
+        Mat out = resized.clone();
+
+        HighGui.imshow("original", resized);
+
+        Mat blur = new Mat();
+        Imgproc.GaussianBlur(resized, blur, new Size(15, 15), 2);
+        HighGui.imshow("blur", blur);
+
+        Mat hsv = new Mat();
+        Imgproc.cvtColor(blur, hsv, Imgproc.COLOR_BGR2HSV);
+        Mat mask = new Mat();
+        Scalar min = new Scalar(20, 100, 100);
+        Scalar max = new Scalar(30, 255, 255);
+        Core.inRange(hsv, min, max, mask);
+        HighGui.imshow("mask", mask);
+
+        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
+        Mat filterred = new Mat();
+        Point anchor = new Point(-1, -1);
+        Imgproc.erode(mask, filterred, kernel, anchor, 2);
+        Imgproc.dilate(filterred, filterred, kernel, anchor, 4);
+        Imgproc.erode(filterred, filterred, kernel, anchor, 4);
+        HighGui.imshow("filtter", filterred);
+
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat(1, 1, CvType.CV_32SC2);
+        Imgproc.findContours(filterred, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+        // Find the best contour based on area and aspect ratio, and draw a bounding
+        // rectangle and center point on the output image
+        MatOfPoint best = null;
+        Rect bestRect = null;
+        double bestRatio = 0;
+        double bestSize = 0;
+        for (int i = 0; i < contours.size(); i++) {
+            MatOfPoint m = contours.get(i);
+            double area = Imgproc.contourArea(m);
+            Rect rect = Imgproc.boundingRect(m);
+            double r = (double) rect.height / rect.width;
+            if (area > 200) {
+                Imgproc.drawContours(out, contours, i, new Scalar(0, 0, 255), 2);
+                if (best == null || isBetter(r, area, bestRatio, bestSize)) {
+                    best = m;
+                    bestRect = rect;
+                    bestSize = area;
+                    bestRatio = r;
+                }
+            }
+        }
+
+        if (bestRect != null) {
+            Imgproc.rectangle(out, bestRect, new Scalar(0, 255, 0), 3);
+            Moments m = Imgproc.moments(best);
+            int centerX = (int) (m.get_m10() / m.get_m00());
+            int centerY = (int) (m.get_m01() / m.get_m00());
+            Imgproc.circle(out, new Point(centerX, centerY), 5, new Scalar(0, 255, 0), -1);
+        }
+        HighGui.imshow("out", out);
+
+        // Wait for a key press and exit
+        HighGui.waitKey(0);
+        HighGui.destroyAllWindows();
+        System.exit(1);
+    }
+
+     private static boolean isBetter(double r, double area, double bestRatio, double bestSize) {
+        if(bestSize == 0) {
+            return true;
+        }
+        double ratioDiff = Math.abs(r - 1);
+        double bestRatioDiff = Math.abs(bestRatio - 1);
+        if(ratioDiff < bestRatioDiff) {
+            return true;
+        } else if(ratioDiff == bestRatioDiff) {
+            return area > bestSize;
+        } else {
+            return false;
+        }
+    }
+
+}
